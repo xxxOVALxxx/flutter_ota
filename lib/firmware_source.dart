23b73d0a5b0152879ad33354d46aefd9105ce7a8
xxxOVALxxx <null>
2023-08-12 19:30:15 +0500

    update UrlFirmwareSource

--- lib/firmware_source.dart
+++ lib/firmware_source.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 import 'other.dart';
 
@@ -34,8 +35,22 @@ class UrlFirmwareSource extends FirmwareSource {
 
   @override
   Future<List<Uint8List>> getFirmwareChunks(int mtuSize) async {
+    const timeout = Duration(seconds: 10);
+    late final Response response;
+
     // fetch and split into chunks
-    final response = await http.get(Uri.parse(url));
+    try {
+      response = await http.get(Uri.parse(url)).timeout(timeout);
+    } catch (e) {
+      // Handle other errors (e.g., timeout, network connectivity issues)
+      throw 'Error fetching firmware from URL: $e';
+    }
+
+    // Handle HTTP error (e.g., status code is not 200)
+    if (response.statusCode != 200) {
+      throw 'HTTP Error: ${response.statusCode} - ${response.reasonPhrase}';
+    }
+
     final bytes = response.bodyBytes;
 
     return splitIntoChunks(bytes, mtuSize);
